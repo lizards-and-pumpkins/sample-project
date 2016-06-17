e9b7e78b365954dc6ef64b3042150a616a480c33
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2016-06-17 08:42:42 +0200

    Issue #1: Add missing magento-connector product images directory configuration

--- src/magento-extensions/LizardsAndPumpkins_DemoTheme/app/code/local/LizardsAndPumpkins/DemoTheme/sql/lizardsandpumpkins_demotheme_setup/upgrade-0.8.0-0.9.0.php
+++ src/magento-extensions/LizardsAndPumpkins_DemoTheme/app/code/local/LizardsAndPumpkins/DemoTheme/sql/lizardsandpumpkins_demotheme_setup/upgrade-0.8.0-0.9.0.php
@@ -6,13 +6,15 @@
 $this->startSetup();
 
 $pathToShare = Mage::getRoot() . '/../../../share';
+$pathToImagesDir = $pathToShare . '/product-images';
 
-mkdir($pathToShare . '/product-images');
+mkdir($pathToImagesDir);
 
 /** @var Mage_Core_Model_Config $config */
 $config = Mage::getModel('core/config');
 
 $config->saveConfig('lizardsAndPumpkins/magentoconnector/local_path_for_product_export', 'file://' . $pathToShare);
 $config->saveConfig('lizardsAndPumpkins/magentoconnector/api_url', 'http://demo.lizardsandpumpkins.com.loc/fr/api/');
+$config->saveConfig('lizardsAndPumpkins/magentoconnector/image_target', $pathToImagesDir);
 
 $this->endSetup();
