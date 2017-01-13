f24f1ee0ac858b2ed24c48ec6fe986ff17171f36
Vinai Kopp <vinai@netzarbeiter.com>
2017-01-13 11:15:03 +0100

    Issue #59: Add cli script to shutdown consumer processes

---
+++ src/lizards-and-pumpkins/bin/shutdownConsumerProcess.php
@@ -0,0 +1,98 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types = 1);
+
+namespace LizardsAndPumpkins;
+
+use League\CLImate\CLImate;
+use LizardsAndPumpkins\Messaging\Consumer\ShutdownWorkerDirective;
+use LizardsAndPumpkins\Util\BaseCliCommand;
+use LizardsAndPumpkins\Util\Factory\CommonFactory;
+use LizardsAndPumpkins\Util\Factory\MasterFactory;
+use LizardsAndPumpkins\Util\Factory\SampleMasterFactory;
+use LizardsAndPumpkins\Util\Factory\DemoProjectFactory;
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+class ShutdownConsumer extends BaseCliCommand
+{
+    /**
+     * @var SampleMasterFactory
+     */
+    private $factory;
+
+    private function __construct(MasterFactory $factory, CLImate $climate)
+    {
+        $this->factory = $factory;
+        $this->setCLImate($climate);
+    }
+
+    public static function bootstrap(): ShutdownConsumer
+    {
+        $factory = new SampleMasterFactory();
+        $factory->register(new CommonFactory());
+        $factory->register(new DemoProjectFactory());
+
+        return new self($factory, new CLImate());
+    }
+
+    /**
+     * @param CLImate $climate
+     * @return array[]
+     */
+    protected function getCommandLineArgumentsArray(CLImate $climate): array
+    {
+        return array_merge(
+            parent::getCommandLineArgumentsArray($climate),
+            [
+                'type' => [
+                    'description' => '"command" or "event"',
+                    'required'    => true,
+                ],
+                'pid'  => [
+                    'description'  => 'numeric PID',
+                    'required'     => true,
+                    'defaultValue' => "any",
+                ],
+            ]
+        );
+    }
+
+    protected function execute(CLImate $climate)
+    {
+        $queue = $this->selectQueue();
+        $queue->add($this->createShutdownDirective());
+        $this->output(sprintf('Shutdown directive for %s consumer with pid "%s" added', $this->type(), $this->pid()));
+    }
+
+    private function type() : string
+    {
+        $type = $this->getArg('type');
+        if ('command' !== $type && 'event' !== $type) {
+            throw new \InvalidArgumentException('Type must be "command" or "event"');
+        }
+
+        return $type;
+    }
+
+    private function pid() : string
+    {
+        return $this->getArg('pid');
+    }
+
+    /**
+     * @return Messaging\Command\CommandQueue|Messaging\Event\DomainEventQueue
+     */
+    private function selectQueue()
+    {
+        return $this->type() === 'command' ? $this->factory->getCommandQueue() : $this->factory->getEventQueue();
+    }
+
+    private function createShutdownDirective(): ShutdownWorkerDirective
+    {
+        return new ShutdownWorkerDirective($this->pid() === 'any' ? '*' : $this->pid());
+    }
+}
+
+ShutdownConsumer::bootstrap()->run();
